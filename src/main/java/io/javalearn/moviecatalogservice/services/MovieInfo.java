5346d7b291aeea8ee0b0d87565deac12f620bdc5
esakkimuthuvelraj <ev@findroids.com>
2020-05-02 12:15:07 +0530

    added property for hystrix execution.isolation.thread.timeoutInMilliseconds circuitBreaker.requestVolumeThreshold circuitBreaker.errorThresholdPercentage circuitBreaker.sleepWindowInMilliseconds

--- src/main/java/io/javalearn/moviecatalogservice/services/MovieInfo.java
+++ src/main/java/io/javalearn/moviecatalogservice/services/MovieInfo.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import io.javalearn.moviecatalogservice.models.Movie;
 
@@ -19,7 +20,13 @@ public class MovieInfo {
 	}
 	
 	
-	@HystrixCommand(fallbackMethod="getFallbackMovie")
+	@HystrixCommand(fallbackMethod="getFallbackMovie",
+			commandProperties= {
+				@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000"),
+				@HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value="5"),
+				@HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value="50"),
+				@HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value="5000")
+			})
 	public Movie getMovie() {
 		return restTemplate.getForObject("http://movie-info-service/movies/12345", Movie.class);
 	}
